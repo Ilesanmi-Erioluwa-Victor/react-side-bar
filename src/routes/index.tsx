08a4aa7a84b04e72463da7ea0e41b9e8c51e8d2c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 11:14:33 +0100

    working on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,15 @@ const generateRoute = (routes: RouteType[]): ReactNode => {
                     {route.element}
                     </PageWrapper>}
             />
+        ) : (
+                <Route
+                    path={route.path}
+                    element={
+                        
+                    }
+                >
+
+                </Route>
         )
     ))
 }
