6d19eb7298d009a4af76ce2599b1300d173c6ac4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 10:59:56 +0100

    working on routes

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,1 +1,25 @@
-const appRoute = {};
+import HomePage from "pages/Home/HomePage";
+import DashboardLayout from "pages/Dashboard/DashboardLayout";
+
+import { RouteType } from "./config";
+import DashboardOutlinedIcon  from "@mui/icons-material/DashboardOutlined";
+
+const appRoutes: RouteType[] = [
+  {
+    index: true,
+    state: "home",
+    element: <HomePage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    state : "Dashboard",
+    sidebarProps: {
+      displayText: "Dashboard",
+      icon : <DashboardOutlinedIcon />
+    }
+  },
+];
+
+
+export default appRoutes;
