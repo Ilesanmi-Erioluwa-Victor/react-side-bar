e8b28a13ab23716476d5048dabf56beff9313398
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 13:48:19 +0100

    Working on sidebar component

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import { Drawer,List, Toolbar, Stack, Avatar} from "@mui/material"
-import sizeConfigs from 'config/sizeConfigs'
-import assets from 'assets'
+import React from "react";
+import { Drawer, List, Toolbar, Stack, Avatar } from "@mui/material";
+import sizeConfigs from "config/sizeConfigs";
+import assets from "assets";
 
 const Sidebar = () => {
   return (
     <Drawer
-      variant='permanent'
+      variant="permanent"
       sx={{
         width: sizeConfigs.sidebar.width,
-        flexShrink : 0
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: sizeConfigs.sidebar.width,
+          boxSizing : "border-box"
+        },
       }}
     >
       <List disablePadding>
-        <Toolbar />
-        <Stack sx={{ width: "100%", }}
-          direction="row"
-          justifyContent="center"
-        >
-           <Avatar src={assets.images.logo} />
-        </Stack>
+        <Toolbar>
+          <Stack
+            sx={{ width: "100%" }}
+            direction="row"
+            justifyContent="center"
+          >
+            <Avatar src={assets.images.logo} />
+          </Stack>
+        </Toolbar>
       </List>
     </Drawer>
-  )
-}
+  );
+};
 
-export default Sidebar
-
+export default Sidebar;
