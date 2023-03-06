52dc6a8c429cb953712fcec9e0dc55649b0f36e1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 09:03:34 +0100

    Working on MainLayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import {Box }
+import { Box } from "@mui/material";
+import Topbar from "components/common/Topbar";
+import sizeConfigs from "config/sizeConfigs";
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Topbar />
+      <Box
+        component={"nav"}
+        sx={{
+          width: sizeConfigs.sidebar.width,
+        flexShrink : 0}}
+      ></Box>
+    </Box>
+  );
 };
 
 export default MainLayout;
