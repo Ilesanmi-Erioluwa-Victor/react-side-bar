f9b8a0874b4ebd9f7a64dec9a8c8cccf148741b0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 09:33:10 +0100

    Done working on Topbar component

--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { AppBar } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import sizeConfigs from "config/sizeConfigs";
+import colorConfigs from "config/colorConfigs";
 
 const Topbar = () => {
-  return (<AppBar
-    position="fixed"
-    sx ={{width : `calc(100% - ${sizeConfigs.sidebar.width})`}}
-  >
-
-
-  </AppBar>);
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: `calc(100% - ${sizeConfigs.sidebar.width})`,
+        ml: sizeConfigs.sidebar.width,
+        boxShadow: "unset",
+        backgroundColor: colorConfigs.topbar.bg,
+        color: colorConfigs.topbar.color,
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6">React Sidebar with Dropdown</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Topbar;
