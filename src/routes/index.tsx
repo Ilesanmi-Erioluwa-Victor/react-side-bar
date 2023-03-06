0030b575aa025b8dd3d309f8d7febe9d0621d475
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 11:09:29 +0100

    working on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
+import { Route } from "react-router-dom";
 import { RouteType } from "./config";
 
 const generateRoute = (routes: RouteType[]): ReactNode => {
-    
+    return routes.map((route, index) => (
+        route.index ? (
+            <Route></Route>
+        )
+    ))
 }
