cda5a821d3a7e8b0e3273d2fc15f354bb43fa250
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 08:31:49 +0100

    Creatting pages

---
+++ src/pages/Changelog/Changelog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Changelog = (props : Props) => {
+  return (
+    <div>
+      Changelog
+    </div>
+  )
+}
+
+export default Changelog
+
---
+++ src/pages/Component/ComponentPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ComponentPage = (props: Props) => {
+  return <div>Component Page..</div>;
+};
+
+export default ComponentPage;
