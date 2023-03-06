1350b4e253934229357e2fbb6ad074267d0bd74c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 15:46:23 +0100

    almost done with project

--- src/redux/features/appStateSlice.ts
+++ src/redux/features/appStateSlice.ts
@@ -9,9 +9,16 @@ const initialState: appState = {
 };
 
 export const appStateSlice = createSlice({
-    name: "appState",
-    initialState,
-    reducers: {
-        setAppState : (state, action: PayloadAction)
-    }
-})
+  name: "appState",
+  initialState,
+  reducers: {
+    setAppState: (state, action: PayloadAction<string>) => {
+      state.appState = action.payload;
+    },
+  },
+});
+
+
+export const {
+
+} = appStateSlice.actions
