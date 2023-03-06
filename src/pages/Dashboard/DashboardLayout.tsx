c5ed9f6b99975c695c11129a2a72d4a3281a533b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 11:29:35 +0100

    working on routes

--- src/pages/Dashboard/DashboardLayout.tsx
+++ src/pages/Dashboard/DashboardLayout.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
-type Props = {};
-
-const DashboardLayout = (props: Props) => {
-  return <div>DashboardLayout..</div>;
+const DashboardLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 };
 
 export default DashboardLayout;
