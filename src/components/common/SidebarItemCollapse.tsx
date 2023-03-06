2053d3b935a0f652b0910581692d6f1e74293860
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 14:35:18 +0100

    Working on sidebar component

--- src/components/common/SidebarItemCollapse.tsx
+++ src/components/common/SidebarItemCollapse.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { ListItemButton, ListItemIcon } from "@mui/material";
+import { Link } from "react-router-dom";
 import { RouteType } from "routes/config";
+import colorConfigs from "config/colorConfigs";
 
 type Props = {
   item: RouteType;
 };
 
 const SidebarItemCollapse = ({ item }: Props) => {
-  return item?.sidebarProps ? <></> : null;
+  const [open, setOpen] = useState<boolean>(false);
+
+  return item?.sidebarProps ? (
+    <>
+      <ListItemButton
+        component={Link}
+        to={item?.path}
+        sx={{
+          "&: hover": {
+            backgroundColor: colorConfigs.sidebar.hoverBg,
+          },
+          paddingY: "12px",
+          paddingX: "24px",
+        }}
+      >
+        <ListItemIcon
+          sx={{
+            color: colorConfigs.sidebar.color,
+          }}
+        >
+          {item?.sidebarProps?.icon}
+        </ListItemIcon>
+        {item.sidebarProps.displayText}
+      </ListItemButton>
+    </>
+  ) : null;
 };
 
 export default SidebarItemCollapse;
