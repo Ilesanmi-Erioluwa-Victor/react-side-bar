b6967f20b800262a1f950fca92ffd730ffa36fdd
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-06 13:39:41 +0100

    Working on sidebar component

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Drawer } from "@mui/material"
+import sizeConfigs from 'config/sizeConfigs'
 
 const Sidebar = () => {
   return (
     <Drawer
       variant='permanent'
       sx={{
-        
+        width: sizeConfigs.sidebar.width,
+        flexShrink : 0
       }}
     >
       Sidebar
